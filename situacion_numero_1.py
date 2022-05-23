309773c14444e99d3ec29e43ea7bd07703224277
Oddboy89 <91582821+Oddboy89@users.noreply.github.com>
2022-05-23 10:27:16 -0500

    Create situacion_numero_1.py

---
+++ situacion_numero_1.py
@@ -0,0 +1,16 @@
+#1.	Escribe un programa que solicité al usuario ingresar cuatro números para luego mostrar el promedio de los cuatro.
+
+########puedes escoger caulquiera de las dos formas
+
+print('forma 1')#forma1
+a = 4
+total = 0
+for i in range(a):
+    b=int(input('ingrese su numero > '))
+    total = total+b
+print(f'El promedio es: {total/a}')
+
+print('forma 2')#forma2
+
+a=int(input('ingrese su numero > '))+int(input("ingrese su numero > "))+int(input("ingrese su numero > "))+int(input("ingrese su numero > 3"))
+print(a)
