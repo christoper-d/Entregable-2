eca5b37a692cbd2f467f294142b16ba19300dd35
Oddboy89 <91582821+Oddboy89@users.noreply.github.com>
2022-05-23 12:07:05 -0500

    Create situacion_numero_2.py

---
+++ situacion_numero_2.py
@@ -0,0 +1,43 @@
+#contribuciones
+juan = 6090
+carlos = 4435
+maria = 2299
+
+#total de lo recaudado
+empresa =(juan+carlos+maria)
+
+
+#operacion para sacar el porcentaje
+porcentaje =((juan/empresa)*100)
+porcentaje2 =((carlos/empresa)*100)
+porcentaje3 =((maria/empresa)*100)
+
+#total de los porcentajes
+total=(porcentaje+porcentaje2+porcentaje3)
+
+
+##salida##
+print()
+print('######### sin resolver ########\n')
+
+print('----------------------------')
+print(f'|Personas|dinero|Porcentaje|')
+print('----------------------------')
+print(f'|JUAN   |  {juan}  |  ?  |')
+print(f'|CARLOS |  {carlos}  |  ?  |')
+print(f'|MARIA  |  {maria}  |  ?  |')
+print('*---------------------------')
+print(f'| Total:| {empresa}  | {round(total)} %   |')
+print('*---------------------------\n')
+
+print('######### RESUELTO ########\n')
+
+print('----------------------------')
+print(f'|Personas|dinero|Porcentaje|')
+print('----------------------------')
+print(f'|JUAN   |  {juan}  |  {round(porcentaje)} %   |')
+print(f'|CARLOS |  {carlos}  |  {round(porcentaje2)} %   |')
+print(f'|MARIA  |  {maria}  |  {round(porcentaje3)} %   |')
+print('*---------------------------')
+print(f'| Total:| {empresa}  | {round(total)} %   |')
+print('*---------------------------')
