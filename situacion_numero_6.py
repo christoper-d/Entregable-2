20292bd7dc6ad2b832be568211fcba9457a05033
Oddboy89 <91582821+Oddboy89@users.noreply.github.com>
2022-05-23 14:58:57 -0500

    Create situacion_numero_6.py

---
+++ situacion_numero_6.py
@@ -0,0 +1,12 @@
+print()
+print('-------------------------')
+print('|  >Ingrese un numeros  |')
+print('-------------------------\n')
+
+num1 =int(input('=> '))
+
+
+if num1 %2==0:
+    print(f'|       | {num1} es un numero par')
+elif num1 %2!=0:
+    print(f'|       | {num1} es un numero impar')
