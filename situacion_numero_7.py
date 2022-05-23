25cc78bd9ee6ee522728022e8b5d65072ada724f
Oddboy89 <91582821+Oddboy89@users.noreply.github.com>
2022-05-23 15:19:49 -0500

    Create situacion_numero_7.py

---
+++ situacion_numero_7.py
@@ -0,0 +1,14 @@
+print()
+print('-------------------------------')
+print('|    EL AREA DEL TRIANGULO    |')
+print('-------------------------------\n')
+
+print('>Ingrese sus numeros  \n')
+
+
+base =int(input(' Base => '))
+altura =int(input(' Altura => '))
+
+resultado =((base*altura)/2)
+print()
+print(f'|       | EL area del triangulo es : {round(resultado)}')
