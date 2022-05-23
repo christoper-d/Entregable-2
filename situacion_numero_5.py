b0d06d303501b46b818c23167defa7fa31bb66eb
Oddboy89 <91582821+Oddboy89@users.noreply.github.com>
2022-05-23 13:32:15 -0500

    Create situacion_numero_5.py

---
+++ situacion_numero_5.py
@@ -0,0 +1,21 @@
+print()
+print('------------------------------')
+print('|   DETECTOR DE NUMEROS      |')
+print('------------------------------\n')
+
+print('>Ingrese sus numeros  \n')
+
+
+num1 =int(input('=> '))
+num2 =int(input('=> '))
+num3 =int(input('=> '))
+
+print()
+list =[num1,num2,num3]
+
+print(f'Numeros ingresados: {list}')
+
+print()
+print('----------------------------------------------------')
+print(f'| El nuemro MAYOR  "{max(list)}", El numero MENOR es "{min(list)}" |')
+print('----------------------------------------------------\n')
