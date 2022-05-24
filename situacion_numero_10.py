cf0e9871192780a359a795a411f75eba61d4f1dd
Oddboy89 <91582821+Oddboy89@users.noreply.github.com>
2022-05-23 21:30:07 -0500

    Create situacion_numero_10.py

---
+++ situacion_numero_10.py
@@ -0,0 +1,34 @@
+print()
+print('-------------------------------')
+print('|    calcular los salarios    |')
+print('-------------------------------\n')
+
+print('      ---Bienvenido---     ')
+
+print()
+print('>Ingresa las horas:  \n')
+horas=int(input('=> '))
+
+print()
+print('>Ingrese el turno(m,t,n):  \n')
+turno=input('=>')
+
+
+salario=horas*37
+
+
+if turno=="m":
+    salario==horas*37
+elif turno=="t":
+    salario+=salario*0.2
+elif turno=="n":
+    salario+=salario*0.5
+
+if salario >=2000 and salario<=5000:
+    salario-=salario*0.15
+elif salario>=8000 and salario<=10000:
+    salario*=salario*0.17
+
+print()
+print("|       |El salario del trabajador es: ",salario)
+
