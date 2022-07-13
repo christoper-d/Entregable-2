c438b9b25f0869da062d894c22baae435f030d14
christoper__d <91582821+christoper-d@users.noreply.github.com>
2022-07-13 11:02:57 -0500

    Update situacion_numero_10.py

--- situacion_numero_10.py
+++ situacion_numero_10.py
@@ -1,15 +1,14 @@
 
-#                             -----IMPORT-----
-#
+#                    -----IMPORT-----
 #-  Eliminar si no se hace desde la termminal  O la CMD, junto a los clearConsole() del codigo
 import os
 
 def clearConsole():
     command = 'clear'
-    if os.name in ('nt', 'dos'):
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
         command = 'cls'
     os.system(command)
-##-------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------
 
 
 #                 -----Main-----
@@ -26,8 +25,7 @@ def main():
         print('>Ingresa las horas:  \n')
         horas=int(input('=[> '))
 
-        print()
-        print('                  '+'+--------------------------+')
+        print('\n','                 '+'+--------------------------+')
         print(' >Ingrese el turno|   m   ||    t   ||   n   |  \n','                 '+'+--------------------------+')
         turno=input('=[>')
         salario=horas*37
@@ -37,18 +35,21 @@ def main():
                 salario==horas*37
                 if salario >=2000 and salario<=5000:
                     salario-=salario*0.15
+                    print()
                     print("|       |El salario del trabajador es: ",salario)
                     print()
             elif turno=="t":
                 salario+=salario*0.2
                 if salario >=2000 and salario<=5000:
                     salario-=salario*0.15
+                    print()
                     print("|       |El salario del trabajador es: ",salario)
                     print()
             elif turno=="n":
                 salario+=salario*0.5  
                 if salario >=2000 and salario<=5000:
                     salario-=salario*0.15
+                    print()
                     print("|       |El salario del trabajador es: ",salario)
                     print()
         except:
@@ -61,6 +62,5 @@ def main():
         print('         ---vuelve a intentarlo xd---  \n')
         main()
 main()
-#-------------------------------------------------------------------------------------
 
-#Autor: CHRISTOPER DIAZ© Año: 2022
+#-------------------------------------------------------------------------------------
